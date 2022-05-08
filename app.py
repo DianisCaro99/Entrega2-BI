9b3e9b31bdd542410664737e4a69c1d35e3902f2
Dianis Caro <dianiscaro@Dianiss-MacBook-Pro.local>
2022-05-08 11:41:23 -0500

    final

--- app.py
+++ app.py
@@ -149,4 +149,4 @@ def make_predictions_r():
     return jsonify(response=['Eligible' if prediction == 1 else 'Not eligible',proba])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
